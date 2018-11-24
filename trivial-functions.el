4bd3c357ec96809416015f751a79889c398b3453
B.V. Raghav <bvraghav@gmail.com>
2018-11-24 16:53:36 +0530

    Add eval-and-replace function.

--- trivial-functions.el
+++ trivial-functions.el
@@ -6,4 +6,14 @@
    (delete-region beg end)
    (insert (nreverse region))))
 
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+	   (insert (current-kill 0)))))
+
 (provide 'trivial-functions)
