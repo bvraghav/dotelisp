24099b8dbf8ad789947a922c8b37710f88c5810b
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2023-02-09 22:42:41 +0000

    Refactor for legibility.

--- get-dpi.el
+++ get-dpi.el
@@ -1,23 +1,22 @@
 (defun get-dpi ()
-  (with-temp-buffer 
+  (with-temp-buffer
+    
     (insert (shell-command-to-string "xrandr"))
     (keep-lines " connected" (point-min) (point-max))
-    (let* ((words (split-string
-                   (buffer-substring-no-properties (point-min)
-                                                   (point-max))
-                   " "))
+    (let* ((line (buffer-substring-no-properties (point-min)
+                                                 (point-max)))
+           (words (split-string line " "))
            (sz (nth 3 words))
-           (sx (string-to-number
-                (first (split-string sz "x"))))
-           (sy (string-to-number
-                (first (split-string
-                        (second (split-string sz "x"))
-                        "+"))))
-           (lx (/ (string-to-number (nth 2 (reverse words)))
-                  25.4))
-           (ly (/ (string-to-number (first (reverse words)))
-                  25.4)))
-      (* 0.5 (+ (/ sx lx) (/ sy ly))))))
+           (sx (first (split-string sz "x")))
+           (sy (second (split-string sz "x")))
+           (sy (first (split-string sy "+")))
+           (lx (caddr (reverse words)))
+           (ly (first (reverse words))))
+      (pcase-let ((`(,sx ,sy ,lx ,ly) (mapcar #'string-to-number 
+                                              `(,sx ,sy ,lx ,ly))))
+        (* 0.5 25.4 (+ (/ sx lx 1.0) (/ sy ly 1.0))))
+      )
+    ))
 
 (defun insert-dpi ()
   (interactive)
