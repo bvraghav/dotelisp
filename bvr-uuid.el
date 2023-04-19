2dab52ffb9b9e26709d255749a6d3af8d48de065
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2023-04-19 18:11:35 +0100

    Multiple updates.
    1. Def group.
    2. Def custom.
    3. Defun to insert random-uuid-string.

--- bvr-uuid.el
+++ bvr-uuid.el
@@ -6,6 +6,18 @@
 ;;; Code:
 (require 'uuidgen)
 
+(use-package bvr-utils)
+
+(defgroup bvr-uuid nil
+  "Properties of 'bvr-uuid package."
+  :group 'bvr-utils)
+
+(defcustom bvr/random-uuid-string-length 8
+  "String length of uuid string to insert."
+  :type '(integer)
+  :local t
+  :group 'bvr-uuid)
+
 (defun bvr/random-seed-from-uuidgen nil
   "Insert random seed int at poing from uuidgen."
   (interactive)
@@ -15,6 +27,20 @@
          (snum (string-join snum))
          (snum (string-to-number snum 16)))
     (insert (number-to-string snum))))
+
+(defun bvr/random-uuid-string (arg)
+  "Insert random string from uuid.
+
+Prompt for string length if prefix ARG is given."
+  (interactive "P")
+  (let* ((slen bvr/random-uuid-string-length)
+         (slen (if (and slen (not arg)) slen
+                 (read-integer "String Length: ")))
+         (snum (uuidgen-4))
+         (snum (split-string snum "-"))
+         (snum (string-join snum))
+         (snum (substring snum 0 slen)))
+    (insert snum)))
 
 (provide 'bvr-uuid)
 ;;; bvr-uuid.el ends here
