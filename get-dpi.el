a8c58f63e7eeb2ae9e8df186d7e292b61e192a23
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2023-06-06 13:48:33 +0100

    Refactor for display not connected (remote connection through tty.)

--- get-dpi.el
+++ get-dpi.el
@@ -1,22 +1,29 @@
+
 (defun get-dpi ()
+  (or (get-dpi-connected) 0))
+
+(defun get-dpi-connected ()
   (with-temp-buffer
     
     (insert (shell-command-to-string "xrandr"))
     (keep-lines " connected" (point-min) (point-max))
-    (let* ((line (buffer-substring-no-properties (point-min)
-                                                 (point-max)))
-           (words (split-string line " "))
-           (n (max (or (cl-position "connected" words :test #'equal) 0)
-                   (or (cl-position "primary" words :test #'equal) 0)))
-           (sz (nth (1+ n) words))
-           (sx (first (split-string sz "[x+]" )))
-           (sy (second (split-string sz "[x+]" )))
-           (lx (caddr (reverse words)))
-           (ly (first (reverse words))))
-      (pcase-let ((`(,sx ,sy ,lx ,ly) (mapcar #'string-to-number 
-                                              `(,sx ,sy ,lx ,ly))))
-        (* 0.5 25.4 (+ (/ sx lx 1.0) (/ sy ly 1.0))))
-      )
+    (when (< 0 (first (wc-count)))
+      (gd--compute-dpi))))
+
+(defun gd--compute-dpi ()
+  (let* ((line (buffer-substring-no-properties (point-min)
+                                               (point-max)))
+         (words (split-string line " "))
+         (n (max (or (cl-position "connected" words :test #'equal) 0)
+                 (or (cl-position "primary" words :test #'equal) 0)))
+         (sz (nth (1+ n) words))
+         (sx (first (split-string sz "[x+]" )))
+         (sy (second (split-string sz "[x+]" )))
+         (lx (caddr (reverse words)))
+         (ly (first (reverse words))))
+    (pcase-let ((`(,sx ,sy ,lx ,ly) (mapcar #'string-to-number 
+                                            `(,sx ,sy ,lx ,ly))))
+      (* 0.5 25.4 (+ (/ sx lx 1.0) (/ sy ly 1.0))))
     ))
 
 (defun insert-dpi ()
