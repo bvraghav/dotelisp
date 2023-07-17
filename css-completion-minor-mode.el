3c431ed97f846d6c8bbaf81ed077f458166c6c46
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2023-07-17 08:18:45 +0100

    Update docs, and refactor for legibility.

--- css-completion-minor-mode.el
+++ css-completion-minor-mode.el
@@ -15,6 +15,11 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; This extension relies on `rg' being installed on the
+;; system.
+
 ;;; Code:
 
 (require 'cl-lib)
@@ -30,45 +35,54 @@
 ;;       (delete-region beg end))
 ;;     (insert new-word)))
 
-(defgroup bvr/css nil "BVR's CSS Helpers group")
+(defgroup bvr/css nil "BVR's CSS Helpers group.")
 (defcustom bvr/css/search-paths '(".")
-  "Set of paths to search. Useful to override project level ignores, like searching for `node_modules/tachyons' in a npm package with `node_modules' ignored under `.gitignore'"
+  "Set of paths to search. 
+
+Useful to override project level ignores, like searching for
+`node_modules/tachyons' in a npm package with `node_modules'
+ignored under `.gitignore'"
   :group 'bvr/css
   :type '(repeat directory))
 
 (defcustom bvr/css/valid-modes '(mhtml-mode)
-  "Set of applicable modes for css class completion-at-point"
+  "Set of applicable modes for css class `completion-at-point'."
   :group 'bvr/css
   :type '(repeat symbol))
 
 (defcustom bvr/css/completion-attributes
   '("class")
-  "Attribute values to apply completion for"
+  "Attribute values to apply completion for."
   :group 'bvr/css
   :type '(repeat string))
 
 (defcustom bvr/css/classname-regex
   "\\.([A-Za-z][-_:A-Z0-9a-z]+)[, {]"
-  "Regex pattern for matching classname in files"
+  "Regex pattern for matching classname in files.
+
+The search is performed using `rg'; and the first (sub)group is
+captured."
   :group 'bvr/css
   :type 'string)
 
 (defconst bvr/css/buffer "*bvr/css/class-list*")
 
 (defun bvr/css/compute-paths ()
-  (string-join
+  "Get space separated search paths.
 
-   (mapcar (lambda (name)
-             (if (file-name-absolute-p name) name
-
-               (concat
-                (projectile-ensure-project (projectile-project-root))
-                name)))
-           bvr/css/search-paths)
-
-   " "))
+Resolve paths relative to the project root."
+  (cl-labels
+      ((project-root ()
+                     (projectile-ensure-project
+                      (projectile-project-root)))
+       (proj-resolve (name)
+                     (if (file-name-absolute-p name) name
+                       (concat (project-root) name))))
+    (let* ((paths (mapcar #'proj-resolve bvr/css/search-paths)))
+      (string-join paths " "))))
 
 (defun bvr/css/class-list ()
+  "Get new-line separated class list."
   (when (get-buffer bvr/css/buffer)
     (with-current-buffer bvr/css/buffer (erase-buffer)))
   (call-process-shell-command
@@ -80,6 +94,7 @@
     (split-string (buffer-string) "\n")))
 
 (defun bvr/css/complete-class ()
+  "Suggest completions for class at point maybe."
   (interactive)
   (cl-destructuring-bind (beg . end)
       (bounds-of-thing-at-point 'symbol)
@@ -102,24 +117,30 @@
 ;;           #'bvr/css/completion-at-point)
 
 (defun bvr/css/point-at-attr-p (attr)
+  "Whether point is at ATTR."
   (looking-back (format "\\<%s=\"[^\"]+" attr)))
 
 (defun bvr/css/any-attr-p (attr &rest attrs)
+  "Whether point is at ATTR or any of the ATTRS."
   (or (bvr/css/point-at-attr-p attr)
       (and attrs
            (apply #'bvr/css/any-attr-p attrs))))
 
 (defun bvr/css/completable-p ()
+  "Whether point is at one of completion attributes."
   (apply #'bvr/css/any-attr-p
          bvr/css/completion-attributes))
 
 (defun bvr/css/completion-filter (cmd)
+  "Shim filter function for `menu-item'.
+
+CMD is a forwarded closure."
   (when (bvr/css/completable-p) cmd))
 
 (defconst bvr/css/complete-maybe
   '(menu-item "" bvr/css/complete-class
               :filter bvr/css/completion-filter)
-  "Optional CSS class completion based on context")
+  "Optional CSS class completion based on context.")
 
 (defconst bvr/css/keymap
   (let ((map (make-sparse-keymap)))
@@ -127,10 +148,11 @@
     (define-key map (kbd "TAB") bvr/css/complete-maybe)
     (define-key map (kbd "<tab>") bvr/css/complete-maybe)
     map)
-  "Keymap for CSS completion minor mode")
+  "Keymap for CSS completion minor mode.")
 
 (define-minor-mode css-completion-minor-mode
-  "Complete css classes in the project"
+  "Complete css classes in the project."
   :keymap bvr/css/keymap)
 
 (provide 'css-completion-minor-mode)
+;;; css-completion-minor-mode.el ends here
